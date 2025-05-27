03354c0f5c8a8c178b8576783631c99e57aa762d
marco <marcoaresu82@gmail.com>
2025-05-27 18:26:17 +0200

    Aggiornamento del README e ristrutturazione della classe View per migliorare la leggibilità e la struttura del codice

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-# Sample Gradle Project
+#PROGETTO DATABASE PALESTRA
 
-This is a repository that can be used as a base to develop an OOP project.
-The tools seen in the lab are pre-integrated in the build file.
+Progetto del corso basi di dati.
--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -1,5 +1,20 @@
 package view;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class View {
-    
+
+    private final JFrame frame;
+    private final JPanel panel;
+
+    public View() {
+        frame = new JFrame("My Application");
+        panel = new JPanel();
+        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+        frame.add(panel);
+        frame.setVisible(true);
+    }
 }
