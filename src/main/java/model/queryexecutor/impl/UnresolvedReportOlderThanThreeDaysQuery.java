d9221eed2edb348fc2c6c3f87ba0152c92063b0d
Pietro Siboni <pietro.siboni3@studio.unibo.it>
2025-08-06 15:39:36 +0200

    Implementazione 'UnresolvedReportsOlderThanThreeDays', query 17

---
+++ src/main/java/model/queryexecutor/impl/UnresolvedReportOlderThanThreeDaysQuery.java
@@ -0,0 +1,34 @@
+package model.queryexecutor.impl;
+
+import controller.Controller;
+import model.queryexecutor.api.QueryExecutor;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
+
+public class UnresolvedReportsOlderThanThreeDays implements QueryExecutor {
+
+    //OP-28	Visualizza le segnalazioni non risolte da più di 3 giorni
+    
+    private final String QUERY =
+    "SELECT * " +
+    "FROM Storico_Segnalazioni s " +
+    "WHERE Stato = ‘Non risolto’ " +
+    "AND s.Data <= DATE_SUB(CURDATE(), INTERVAL 3 DAY) ";
+
+    @Override
+    public Optional<ResultSet> execute() {
+        try {
+            Connection connection = java.sql.DriverManager.getConnection(Controller.DATABASE_URL);
+            PreparedStatement statement = connection.prepareStatement(QUERY);
+            ResultSet resultSet = statement.executeQuery();
+            return Optional.of(resultSet);
+        } catch(final SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return Optional.empty();
+    }
+}
