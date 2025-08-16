0d444f6be3734d6a50b6b2ae3c7a494c9dee097d
Pietro Siboni <pietro.siboni3@studio.unibo.it>
2025-08-16 16:19:04 +0200

    Refactor expiring subscriptions query

--- src/main/java/model/queryexecutor/impl/SubscriptionsExpiringQuery.java
+++ src/main/java/model/queryexecutor/impl/SubscriptionsExpiringQuery.java
@@ -6,14 +6,17 @@ import model.queryexecutor.api.Query;
 import java.sql.*;
 import java.util.Optional;
 
+import javax.sql.rowset.CachedRowSet;
+import javax.sql.rowset.RowSetProvider;
+
 public class SubscriptionsExpiringQuery implements Query {
 
     //OP-23	Visualizza gli abbonamenti che scadono entro 7 giorni
 
     private final String QUERY =
-    "SELECT * " +
+    "SELECT *, DATEDIFF(DATE_ADD(Data_stipulazione, INTERVAL Durata DAY), CURDATE()) AS GiorniResidui " +
     "FROM abbonamento_utente " +
-    "WHERE DATEDIFF(DATE_ADD(Data_stipulazione, Durata), CURDATE()) <= 7 ";
+    "WHERE DATEDIFF(DATE_ADD(Data_stipulazione, INTERVAL Durata DAY), CURDATE()) BETWEEN 0 AND 7 ";
     private final Connection connection;
 
     public SubscriptionsExpiringQuery(final Connection connection) {
@@ -26,12 +29,18 @@ public class SubscriptionsExpiringQuery implements Query {
                 PreparedStatement statement = this.connection.prepareStatement(QUERY);
         ){
             final ResultSet resultSet = statement.executeQuery();
-            if(resultSet.next()) {
-                return Optional.of(resultSet);
+            
+            CachedRowSet crs = RowSetProvider.newFactory().createCachedRowSet();
+            crs.populate(resultSet);
+
+            if (!crs.isBeforeFirst()) {
+                return Optional.empty();
             }
+            return Optional.of(crs);
+
         } catch (final SQLException e) {
             throw new RuntimeException(e);
         }
-        return Optional.empty();
+        
     }
 }
